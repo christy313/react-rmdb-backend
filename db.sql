2a1c37641b7b4ce247f775b1bcfb5de8bb966762
christy313 <christycreativity@gmail.com>
2022-10-29 19:57:05 -0400

    First commit back end

---
+++ db.sql
@@ -0,0 +1,19 @@
+CREATE TABLE favlist(
+  movieId string
+  movieTitle VARCHAR(255), string
+  userEmail unique relation to other table login email 
+);
+
+
+CREATE TABLE login(
+  id serial PRIMARY KEY,
+  hash VARCHAR(100) NOT NULL,
+  username VARCHAR(100),
+  email text UNIQUE NOT NULL
+);
+
+CREATE TABLE users(
+  id serial PRIMARY KEY,
+  username VARCHAR(100),
+  email text UNIQUE NOT NULL
+);
