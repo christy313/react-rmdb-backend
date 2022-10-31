4595b41878e8d6b39880b186e96e5e5235703469
christy313 <christycreativity@gmail.com>
2022-10-30 21:17:53 -0400

    Revise db to pool

--- controllers/signup.js
+++ controllers/signup.js
@@ -1,4 +1,4 @@
-const handleSignup = (req, res, db, bcrypt) => {
+const handleSignup = (req, res, pool, bcrypt) => {
   const { username, email, password } = req.body;
 
   if (!email || !username || !password) {
@@ -8,28 +8,30 @@ const handleSignup = (req, res, db, bcrypt) => {
   const saltRounds = 10;
   const hash = bcrypt.hashSync(password, saltRounds);
 
-  db.transaction((trx) => {
-    trx
-      .insert({
-        hash: hash,
-        email: email,
-      })
-      .into("login")
-      .returning("email")
-      .then((loginEmail) => {
-        return trx("users")
-          .returning("*")
-          .insert({
-            username: username,
-            email: loginEmail[0].email,
-          })
-          .then((user) => {
-            res.send(user[0]);
-          });
-      })
-      .then(trx.commit)
-      .catch(trx.rollback);
-  }).catch((err) => res.status(400).json("unable to sign up"));
+  pool
+    .transaction((trx) => {
+      trx
+        .insert({
+          hash: hash,
+          email: email,
+        })
+        .into("login")
+        .returning("email")
+        .then((loginEmail) => {
+          return trx("users")
+            .returning("*")
+            .insert({
+              username: username,
+              email: loginEmail[0].email,
+            })
+            .then((user) => {
+              res.send(user[0]);
+            });
+        })
+        .then(trx.commit)
+        .catch(trx.rollback);
+    })
+    .catch((err) => res.status(400).json("unable to sign up"));
 };
 
 module.exports = {
--- index.js
+++ index.js
@@ -3,18 +3,21 @@ const cors = require("cors");
 const bcrypt = require("bcrypt");
 const knex = require("knex");
 const app = express();
+const pg = require("pg");
 
 const { handleSignup } = require("./controllers/signup");
 const { handleLogin } = require("./controllers/login");
 
-const db = knex({
-  client: "pg",
-  connection: process.env.DATABASE_URL,
-  // connection: {
-  //   connectionString: process.env.DATABASE_URL,
-  //   ssl: true,
-  // },
-});
+const pool = new pg.Pool();
+
+// const db = knex({
+//   client: "pg",
+//   connection: process.env.DATABASE_URL,
+//   // connection: {
+//   //   connectionString: process.env.DATABASE_URL,
+//   //   ssl: true,
+//   // },
+// });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -22,9 +25,9 @@ app.use(cors());
 
 app.get("/", (req, res) => res.status(200).json("success"));
 
-app.post("/login", (req, res) => handleLogin(req, res, db, bcrypt));
+app.post("/login", (req, res) => handleLogin(req, res, pool, bcrypt));
 
-app.post("/signup", (req, res) => handleSignup(req, res, db, bcrypt));
+app.post("/signup", (req, res) => handleSignup(req, res, pool, bcrypt));
 
 app.listen(process.env.PORT || 7858, () => {
   console.log(`server is running on PORT: ${process.env.PORT}`);
