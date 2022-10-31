ab28dd1d47094be69261af5e5b9cef87dfc54d89
christy313 <christycreativity@gmail.com>
2022-10-31 14:31:29 -0400

    Try knex again

--- index.js
+++ index.js
@@ -14,11 +14,21 @@ const { handleLogin } = require("./controllers/login");
 
 // const pool = new pg.Pool();
 
+// const db = knex({
+//   client: "pg",
+//   connection: {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: true,
+//   },
+// });
+
 const db = knex({
   client: "pg",
   connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
+    host: "process.env.PGHOST",
+    user: "process.env.PGUSER",
+    password: "process.env.PGPASSWORD",
+    database: "process.env.PGDATABASE",
   },
 });
 
@@ -32,6 +42,6 @@ app.post("/login", (req, res) => handleLogin(req, res, db, bcrypt));
 
 app.post("/signup", (req, res) => handleSignup(req, res, db, bcrypt));
 
-app.listen(process.env.PORT || 7858, () => {
-  console.log(`server is running on PORT: ${process.env.PORT}`);
+app.listen(process.env.PGPORT || 7858, () => {
+  console.log(`server is running on PORT: ${process.env.PGPORT}`);
 });
