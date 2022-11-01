9e7da151eb17a56f282f1285dc4ab3ae04e4ca88
christy313 <christycreativity@gmail.com>
2022-11-01 17:05:39 -0400

    Add cors function

--- index.js
+++ index.js
@@ -28,7 +28,15 @@ db.connect();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
-app.options("*", cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/", (req, res) => res.status(200).json("success"));
 
