9dd77052b163b1c46376463680798bf8be7e2cba
christy313 <christycreativity@gmail.com>
2022-11-02 16:27:19 -0400

    Comment node tls out

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ const { handleLogin } = require("./controllers/login");
 
 // heroku
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 const db = knex({
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
-    // ssl: true,
+    ssl: true,
   },
 });
 
