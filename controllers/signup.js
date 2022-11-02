c52c0d8f2c5486afde1b83d96f73edd2e5e433e7
christy313 <christycreativity@gmail.com>
2022-11-01 21:54:28 -0400

    Revise transation

--- controllers/signup.js
+++ controllers/signup.js
@@ -8,7 +8,7 @@ const handleSignup = (req, res, db, bcrypt) => {
   const saltRounds = 10;
   const hash = bcrypt.hashSync(password, saltRounds);
 
-  db.runTransaction((trx) => {
+  db.transaction((trx) => {
     trx
       .insert({
         hash: hash,
