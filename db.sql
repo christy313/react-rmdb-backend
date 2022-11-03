1e5398828c2767dc4e05bd0662b639743231cd8a
christy313 <christycreativity@gmail.com>
2022-11-03 10:42:53 -0400

    Noted db format and delete unused files

--- db.sql
+++ db.sql
@@ -1,10 +1,11 @@
+-- Future usage
 CREATE TABLE favlist(
   movieId string
   movieTitle VARCHAR(255), string
   userEmail unique relation to other table login email 
 );
 
--- heroku
+-- Heroku
 -- CREATE TABLE login(
 --   id serial PRIMARY KEY,
 --   hash VARCHAR(100) NOT NULL,
@@ -17,3 +18,19 @@ CREATE TABLE favlist(
 --   username VARCHAR(100),
 --   email text UNIQUE NOT NULL
 -- );
+
+-- Railway
+CREATE TABLE login(
+  id serial PRIMARY KEY,
+  hash text NOT NULL,
+  username text,
+  email text UNIQUE
+);
+
+CREATE TABLE users(
+  id serial PRIMARY KEY,
+  username text,
+  email text UNIQUE
+);
+
+-- Note: there's no VARCHAR in railway, use text instead
